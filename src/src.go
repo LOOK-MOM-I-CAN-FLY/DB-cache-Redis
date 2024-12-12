cb1bafdd176a76be9cc37cd6fd1e71cd2feb97ad
NEKRASOV <146654192+LOOK-MOM-I-CAN-FLY@users.noreply.github.com>
2024-12-12 16:02:43 +0300

    Update src.go

--- src/src.go
+++ src/src.go
@@ -10,7 +10,7 @@ import (
 	"time"
 
 	"github.com/go-redis/redis/v8"
-	_ "github.com/lib/pq" // Импорт PostgreSQL драйвера
+	_ "github.com/lib/pq"
 )
 
 const (
@@ -26,7 +26,7 @@ type App struct {
 func NewApp(dbConnectionString, redisAddr, redisPassword string) *App {
 	ctx := context.Background()
 
-	// Подключение к PostgreSQL
+
 	db, err := sql.Open("postgres", dbConnectionString)
 	if err != nil {
 		log.Fatalf("Failed to connect to the database: %v", err)
@@ -35,7 +35,7 @@ func NewApp(dbConnectionString, redisAddr, redisPassword string) *App {
 		log.Fatalf("Error to ping the database: %v", err)
 	}
 
-	// Подключение к Redis
+
 	redisClient := redis.NewClient(&redis.Options{
 		Addr:     redisAddr,
 		Password: redisPassword,
@@ -50,7 +50,7 @@ func NewApp(dbConnectionString, redisAddr, redisPassword string) *App {
 }
 
 func (app *App) QueryWithCache(query string) (string, error) {
-	// Проверка данных в кэше
+
 	cachedResult, err := app.Cache.Get(app.Ctx, query).Result()
 	if err == nil {
 		return formatAsTable(cachedResult), nil
@@ -58,14 +58,14 @@ func (app *App) QueryWithCache(query string) (string, error) {
 		return "", fmt.Errorf("failed to check Redis cache: %v", err)
 	}
 
-	// Выполнение SQL-запроса
+
 	rows, err := app.DB.Query(query)
 	if err != nil {
 		return "", fmt.Errorf("failed to execute query: %v", err)
 	}
 	defer rows.Close()
 
-	// Получение списка колонок
+
 	columns, err := rows.Columns()
 	if err != nil {
 		return "", fmt.Errorf("failed to fetch columns: %v", err)
@@ -90,19 +90,19 @@ func (app *App) QueryWithCache(query string) (string, error) {
 		results = append(results, row)
 	}
 
-	// Преобразование результата в JSON для хранения в кэше
+
 	resultJSON, err := json.Marshal(results)
 	if err != nil {
 		return "", fmt.Errorf("failed to cache result: %v", err)
 	}
 
-	// Сохранение в кэш
+
 	err = app.Cache.Set(app.Ctx, query, resultJSON, cacheExpiration).Err()
 	if err != nil {
 		return "", fmt.Errorf("failed to save to Redis: %v", err)
 	}
 
-	// Преобразование результата в текст для вывода
+
 	return formatAsTable(string(resultJSON)), nil
 }
 
@@ -113,23 +113,23 @@ func formatAsTable(jsonData string) string {
 		return "Error formatting data"
 	}
 
-	// Форматирование в виде таблицы
+
 	var result string
 	if len(data) == 0 {
 		return "No results"
 	}
 
-	// Получаем имена колонок
+
 	var headers []string
 	for key := range data[0] {
 		headers = append(headers, key)
 	}
 
-	// Формируем заголовок таблицы
+
 	result += fmt.Sprintf(" %-20s\n", strings.Join(headers, " "))
 	result += strings.Repeat("-", 20*len(headers)) + "\n"
 
-	// Добавляем строки
+
 	for _, row := range data {
 		var line string
 		for _, header := range headers {
